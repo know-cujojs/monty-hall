86d4cc186d7226211fbbcd9e8a06fa00939165e2
unscriptable <john@unscriptable.com>
2012-10-01 15:17:32 -0400

    changed config to use a package to configure curl and updated to use dev branch

--- client/app/run.js
+++ client/app/run.js
@@ -4,10 +4,8 @@
 	var config = {
 		baseUrl: '',
 		pluginPath: 'curl/plugin',
-		paths: {
-			curl: 'lib/curl/src/curl'
-		},
-		packages: [{"name":"wire","location":"lib/wire","main":"wire"},{"name":"when","location":"lib/when","main":"when"},{"name":"meld","location":"lib/meld","main":"meld"},{"name":"poly","location":"lib/poly","main":"./poly"}],
+		paths: {},
+		packages: [{"name":"wire","location":"lib/wire","main":"./wire"},{"name":"when","location":"lib/when","main":"when"},{"name":"meld","location":"lib/meld","main":"meld"},{"name":"poly","location":"lib/poly","main":"./poly"},{"name":"curl","location":"lib/curl/src/curl","main":"../curl"}],
 		preloads: ['poly/all']
 	};
 
