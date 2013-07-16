990a12782137ebd390bb220d87351ca364926b34
Scott Andrews <scothis@gmail.com>
2013-07-16 13:33:25 -0400

    point at production url

--- client/app/game/rest.js
+++ client/app/game/rest.js
@@ -7,7 +7,7 @@ define({
 		create: 'app/game/restApi',
 		properties: {
 			gameClient: { $ref: 'gameClient' },
-			host: 'https://monty-hall-backend.cfapps.io/games'
+			host: 'http://monty-hall-backend.demo.cujojs.com/games'
 		}
 	},
 
